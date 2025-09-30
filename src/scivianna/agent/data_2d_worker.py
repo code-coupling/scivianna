2cf440ecab5d0ed2299b155abd42be6cf57d5fae
tmoulignier <thibault.moulignier@cea.fr>
2025-09-30 07:23:32 +0200

    worker example

--- src/scivianna/agent/data_2d_worker.py
+++ src/scivianna/agent/data_2d_worker.py
@@ -55,9 +55,44 @@ class Data2DWorker:
         assert alphas.max() <= 255, f"The values must be lower than 255, found in array {alphas.max()}."
         assert alphas.min() >= 0, f"The values must be greater than 0, found in array {alphas.min()}."
 
-        self.data2d.cell_colors[:, -1] = alphas.astype(int)
+        colors = np.array(self.data2d.cell_colors)
+        colors[:, -1] = alphas.astype(int)
+
+        self.data2d.cell_colors = colors.tolist()
 
     def reset(self,):
         """Returns to the data2d provided in the initialization
         """
         self.data2d = self.data2d_save.copy()
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    # Field example
+    import scivianna
+    from scivianna.constants import GEOMETRY, X, Y
+    from scivianna.interface.med_interface import MEDInterface
+    from scivianna.slave import set_colors_list
+    from scivianna.data import Data2D
+    from scivianna.agent.data_2d_worker import Data2DWorker
+
+    med = MEDInterface()
+    med.read_file(
+        str(Path(scivianna.__file__).parent / "default_jdd" / "power.med"),
+        GEOMETRY,
+    )
+    data_2d:Data2D
+    data_2d, _ = med.compute_2D_data(
+        X,
+        Y, 
+        0, 1, 0, 1, 0, 0, # values not used
+        0., 
+        None,
+        {}
+    )
+    set_colors_list(data_2d, med, "INTEGRATED_POWER", "viridis", False, {})
+
+    worker = Data2DWorker(data_2d)
+
+    worker.set_alpha(np.ones((len(data_2d.cell_colors)))*0.5)
