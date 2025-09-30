206783a8637f1c3411da959b1cd52ed07791872c
tmoulignier <thibault.moulignier@cea.fr>
2025-09-30 08:39:13 +0200

    Bokeh grid display edge contour

--- src/scivianna/plotter_2d/grid/bokeh.py
+++ src/scivianna/plotter_2d/grid/bokeh.py
@@ -33,6 +33,9 @@ import os
 class Bokeh2DGridPlotter(Plotter2D):
     """2D geometry plotter based on the bokeh python module"""
 
+    display_edges = True
+    """Display grid cells edges"""
+
     def __init__(
         self,
     ):
@@ -311,7 +314,38 @@ class Bokeh2DGridPlotter(Plotter2D):
         value_array = np.array([value_map[val] for val in vals])
         color_array = np.array([color_map[val] for val in vals])
 
-        colors = color_array[inv].reshape((*grid.shape, 4))  # shape (n, m, 4)
+        colors = color_array[inv]  # shape (n, m, 4)
+
+        if self.display_edges:
+            flat_data = grid.flatten()
+            roll_1_0 = np.where(flat_data == np.roll(flat_data, -1), 1, 0)
+            roll_1_1 = np.where(flat_data == np.roll(flat_data, 1), 1, 0)
+            contour_1_0 = roll_1_0.reshape(grid.shape)
+            contour_1_1 = roll_1_1.reshape(grid.shape)
+
+            flat_data_2 = grid.T.flatten()
+            roll_2_0 = np.where(flat_data_2 == np.roll(flat_data_2, -1), 1, 0)
+            roll_2_1 = np.where(flat_data_2 == np.roll(flat_data_2, 1), 1, 0)
+
+            contour_2_0 = roll_2_0.reshape(grid.T.shape).T
+            contour_2_1 = roll_2_1.reshape(grid.T.shape).T
+
+            borders = np.expand_dims(np.minimum(
+                    np.minimum(contour_1_0, contour_2_0),
+                    np.minimum(contour_1_1, contour_2_1),
+                ).flatten(), axis=-1)
+            
+            borders = np.concatenate([borders, borders, borders, borders], axis=1)
+
+            edge_color_array = get_edges_colors(color_array)
+
+            edge_colors = edge_color_array[inv]  # shape (n, m, 4)
+
+            colors = np.where(borders == (1, 1, 1, 1), colors, edge_colors).reshape((*grid.shape, 4))
+        
+        else:
+            colors = colors.reshape((*grid.shape, 4))
+
         val_grid = value_array[inv].reshape(grid.shape)
         
         img = np.empty(grid.shape, dtype=np.uint32)
